1b0544136bc0c27ab7f9b9ca3e4a0f02e23dead9
LuisMario10 <125088392+LuisMario10@users.noreply.github.com>
2023-03-07 21:14:43 -0300

    Fiz exercicios do Modulo D do curso de JAVASCRIPT do CURSO EM VIDEO

---
+++ Aprendendo/exercicios modulo D/JS/scriptex2.js
@@ -0,0 +1,22 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    // var hora = data.getHours()
+    var hora = 20
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        img.style.background = 'gold'
+        document.body.style.background = 'yellow'
+    }
+    else if (hora >= 12 && hora <= 18) {
+        img.style.background = 'darkorange'
+        document.body.style.background = 'orangered'
+    }
+    else {
+        img.style.background = 'grey'
+        document.body.style.background = 'darkslategrey'
+
+    }
+
+}
